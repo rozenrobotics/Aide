46a31e8d2e47581953165400cd974aa37b1f8b8d
mikhail shafiev <m_shafiev@list.ru>
2025-02-24 01:26:00 +1000

    b

--- users/models.py
+++ users/models.py
@@ -50,7 +50,7 @@ class RobotProfile(models.Model):
 
 class TicketOrder(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    train_cruise = models.OneToOneField(TrainCruise, on_delete=models.CASCADE, null=True, blank=True)
+    train_cruise = models.OneToOneField(TrainCruise, on_delete=models.CASCADE, null=True, blank=True, unique=False)
     seat_number = models.IntegerField(default=0)
     is_active = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
